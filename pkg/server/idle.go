1fa1dab561db7d80dff98203d04f061b83ef7bf6
Axenide <axenide@gmail.com>
2026-02-25 01:37:41 -0300

    idle

--- pkg/server/idle.go
+++ pkg/server/idle.go
@@ -178,36 +178,65 @@ func (im *IdleManager) waitResumeInternal(timeoutMs uint32, inputOnly bool) erro
 	im.wlMu.Lock()
 	var notif *ext_idle_notify_v1.ExtIdleNotificationV1
 	var err error
+
+	// A timeout of 0 means we get notified the exact moment the seat is inactive.
+	// Since humans can't input continuously at the microsecond level, a 0ms listener
+	// will almost immediately fire 'idled' if the user isn't actively moving the mouse.
+	// Then, the moment they move it, it fires 'resumed'.
+	// This makes 'resume-wait' perfectly reliable regardless of the original timeout!
 	if inputOnly {
-		notif, err = im.notifier.GetInputIdleNotification(timeoutMs, im.seat)
+		notif, err = im.notifier.GetInputIdleNotification(0, im.seat)
 	} else {
-		notif, err = im.notifier.GetIdleNotification(timeoutMs, im.seat)
+		notif, err = im.notifier.GetIdleNotification(0, im.seat)
 	}
 
-	if err == nil {
-		notif.SetIdledHandler(func(e ext_idle_notify_v1.ExtIdleNotificationV1IdledEvent) {
-			// DO NOTHING. We just need to handle it so it doesn't cause issues if we receive it.
-			// Actually wait, if the notification is NOT currently idle, the compositor will NOT send a resumed event
-			// when we move the mouse. It will ONLY send a resumed event AFTER it has sent an idled event!
-		})
+	if err != nil {
+		im.wlMu.Unlock()
+		return err
+	}
 
-		notif.SetResumedHandler(func(e ext_idle_notify_v1.ExtIdleNotificationV1ResumedEvent) {
+	var hasIdled bool
+	notif.SetIdledHandler(func(e ext_idle_notify_v1.ExtIdleNotificationV1IdledEvent) {
+		im.mu.Lock()
+		hasIdled = true
+		im.mu.Unlock()
+	})
+
+	notif.SetResumedHandler(func(e ext_idle_notify_v1.ExtIdleNotificationV1ResumedEvent) {
+		im.mu.Lock()
+		ready := hasIdled
+		im.mu.Unlock()
+		if ready {
 			select {
 			case ch <- struct{}{}:
 			default:
 			}
-		})
-	}
+		}
+	})
 
-	// Wait, we need to guarantee that the compositor considers this object "idled" before it can send "resumed".
-	// The problem in your test script is: you do input-idle-wait (which creates an object, waits for idled, then DESTROYS IT).
-	// Then you do input-resume-wait (which creates a NEW object, and waits for resumed).
-	// But because it's a new object, it starts as "not idle"! So when you move the mouse, the compositor says "it's not idle anyway, so no resumed event".
-	// To fix this, waitResumeInternal MUST wait for the idled event FIRST if it hasn't happened!
+	callback, _ := im.display.Sync()
+	done := make(chan struct{})
+	callback.SetDoneHandler(func(e client.CallbackDoneEvent) { close(done) })
 	im.wlMu.Unlock()
 
-	if err != nil {
-		return err
+	select {
+	case <-done:
+	case <-time.After(2 * time.Second):
+	}
+
+	// We wait briefly. If 'idled' hasn't fired for a 0ms timer, the user is ACTIVELY
+	// spamming the keyboard/mouse right now. Thus, the system is already resumed.
+	time.Sleep(50 * time.Millisecond)
+
+	im.mu.Lock()
+	currentlyIdle := hasIdled
+	im.mu.Unlock()
+
+	if !currentlyIdle {
+		im.wlMu.Lock()
+		notif.Destroy()
+		im.wlMu.Unlock()
+		return nil
 	}
 
 	defer func() {
@@ -216,18 +245,8 @@ func (im *IdleManager) waitResumeInternal(timeoutMs uint32, inputOnly bool) erro
 		im.wlMu.Unlock()
 	}()
 
-	// Wait, waitResume blocks until resumed. If the object was just created, it will first become idle,
-	// THEN become resumed. But the script expects it to be ALREADY idle.
-	// Since Wayland events are asynchronous, the object will immediately fire 'idled' if the time has already passed.
-	// Then when we move the mouse, it fires 'resumed'.
-	// So we just need to wait for resumed!
-
 	<-ch
 	return nil
-}
-
-func (im *IdleManager) WaitResume(timeoutMs uint32) error {
-	return im.waitResumeInternal(timeoutMs, false)
 }
 
 func (im *IdleManager) isIdleInternal(timeoutMs uint32, inputOnly bool) (bool, error) {
@@ -255,6 +274,8 @@ func (im *IdleManager) isIdleInternal(timeoutMs uint32, inputOnly bool) (bool, e
 	// dispatcher might receive the event immediately!
 	notif.SetIdledHandler(func(e ext_idle_notify_v1.ExtIdleNotificationV1IdledEvent) {
 		isIdle = true
+	})
+	notif.SetResumedHandler(func(e ext_idle_notify_v1.ExtIdleNotificationV1ResumedEvent) {
 	})
 
 	// To avoid blocking forever if something goes wrong with Wayland sync
@@ -308,4 +329,8 @@ func (im *IdleManager) WaitInputResume(timeoutMs uint32) error {
 
 func (im *IdleManager) IsInputIdle(timeoutMs uint32) (bool, error) {
 	return im.isIdleInternal(timeoutMs, true)
+}
+
+func (im *IdleManager) WaitResume(timeoutMs uint32) error {
+	return im.waitResumeInternal(timeoutMs, false)
 }
--- test_idle_watch.sh
+++ test_idle_watch.sh
@@ -10,19 +10,11 @@ echo "------------------------------------------"
 
 while true; do
     echo "👁️  Esperando inactividad de $((TIMEOUT / 1000))s... (Deja de tocar el teclado/ratón)"
-    
-    # Esto bloqueará hasta que pase el tiempo configurado sin actividad
     $AXCTL system input-idle-wait $TIMEOUT
-    
-    # Cuando pasamos esta línea, significa que se detectó la inactividad
     echo "💤 [$(date +%H:%M:%S)] ¡Inactividad detectada! Sistema en idle."
     
     echo "🖱️  Esperando actividad... (Mueve el ratón para continuar)"
-    
-    # Esto bloqueará hasta que haya actividad de nuevo
     $AXCTL system input-resume-wait $TIMEOUT
-    
-    # Cuando pasamos esta línea, significa que se detectó actividad
     echo "⚡ [$(date +%H:%M:%S)] ¡Actividad detectada! Has vuelto."
     echo "------------------------------------------"
 done
