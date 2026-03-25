0bbe993acca9519d47bf0673f1c0d273ec38969c
Axenide <axenide@gmail.com>
2026-03-25 17:48:11 -0300

    flake

--- flake.nix
+++ flake.nix
@@ -21,13 +21,13 @@
         {
           default = pkgs.buildGoModule {
             pname = "axctl";
-            version = "0.0.8";
+            version = "0.0.9";
             src = self;
             go = pkgs.go;
             subPackages = [ "." ];
             ldflags = [
               "-X"
-              "main.Version=0.0.8"
+              "main.Version=0.0.9"
             ];
             vendorHash = "sha256-4PUs37IRhUPtuXi4KU8wOUErIkVlcnaoj94zBDBsMdk=";
           };
