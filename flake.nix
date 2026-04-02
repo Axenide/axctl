55d31658b5e6606021a49695e35ff7156bb74ebf
Axenide <axenide@gmail.com>
2026-04-01 22:32:35 -0300

    flake

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         {
           default = pkgs.buildGoModule {
             pname = "axctl";
-            version = "0.0.10";
+            version = "0.0.11";
             src = self;
             go = pkgs.go;
             subPackages = [ "." ];
